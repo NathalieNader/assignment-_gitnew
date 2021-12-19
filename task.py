a455b3161145789cde2d78739312c6a9b401f9e0
Nardin21 <nardinadel10@gmail.com>
2021-12-19 22:51:31 +0200

    git commit -m

--- task.py
+++ task.py
@@ -5,5 +5,8 @@ op =input("Enter any operation +,-: ")
 if op=='+':
     result=num1+num2
     print (num1, "+" , num2, "=", result)
+elif op=='-':
+    result=num1-num2
+    print (num1, "-" , num2, "=", result)
 else:
-    print ("Input character is not recognized!")
+    print ("Input characters not recognized!")
